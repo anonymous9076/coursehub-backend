dae7acfee80cc1c0fa5c8d7568979ee3b7557869
anonymous9076 <vijaytushar915@gmail.com>
2026-01-25 18:43:28 +0530

    ready for vercel

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const errorMiddleware = require('./middleware/error')
 const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
+
+// config 
+dotenv.config({ path: 'config/config.env' })
+connectDatabase();
+
 const app = express();
 
 // route imports 
@@ -14,12 +21,16 @@ const categoryRoute = require('./routes/categoryRoutes')
 const contactRoute = require('./routes/contactRoutes')
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://course-hub-alpha.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use(express.json())
 app.use(cookieParser())
+
+app.get("/", (req, res) => {
+    res.send("Server is working");
+});
 
 app.use("/diginotes", userRoute);
 app.use("/diginotes", courseRoute);
